6d6dff581d01763ec85ab302c4d0b41896dc0646
paddie <keepsmiling@outlook.com>
2026-06-21 21:12:34 +0800

    调整全局样式引入顺序，全局拦截移动端横向滑动，重新打包dist

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,11 @@
 import { createApp} from 'vue'
+import '@/style/index.scss'
 import './style.css'
+
 import App from './App.vue'
 import { createPinia } from 'pinia'
 import router from './router'
 import { Icon } from 'vant'
-// 引入全局样式
-import '@/style/index.scss'
-import { useThemeStore } from './store/modules/theme.ts'
 import 'vant/es/icon/style/index'
 import 'highlight.js/styles/github.css';
 import 'highlight.js/styles/github-dark.css';
@@ -18,7 +17,5 @@ app.use(Icon)
 app.mount('#app')
 
 // 初始化主题
+import { useThemeStore } from './store/modules/theme.ts'
 useThemeStore().initTheme()
-
-
-
